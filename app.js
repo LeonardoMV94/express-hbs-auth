d506495e912d08232d8d228c488a38ec73bd28e1
JRejas <rejas.jose@gmail.com>
2024-12-13 16:06:55 -0300

    pagina olvide-password

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const routes = require('./routers/router')
 const middlewares = require('./middlewares/index')
 const config = require('./config')
 
+const bodyParser = require('body-parser');//agregado por jose
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));  //agregado por jose // Para formularios con datos tipo x-www-form-urlencoded
+
 const port = config.port
 
 middlewares(app)
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -32,6 +32,19 @@ router.post('/register', (req, res) => {
 
 })
 
+router.get('/olvide-password', (req, res) => {
+    res.render('auth/olvide-password',{
+        layout: 'base',
+        estilo: "/css/olvide-pass.css",
+        titulo: "Recuperar Contraseña"
+    });
+})
+
+router.post('/olvide-password', (req, res) => {
+    const { email } = req.body;
+    //Aqui deberia partir la parte de codigo que corresponde a Esteban, el email del usuario ya esta en la variable email
+    res.send(`Proceso de reinicio de contraseña para: ${email}`);
+})
 
 
 module.exports = router
