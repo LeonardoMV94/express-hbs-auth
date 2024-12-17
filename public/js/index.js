178aa7bad2cead9f21b5de14c44e8c210511e871
FranciscoM <francis.joaquin1989@gmail.com>
2024-12-16 23:29:22 -0300

    Validacion confirmar-cuenta

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,13 @@
-import validacionRegistro from './validacionRegistro.js'
-// import validacionLogin from './validacionLogin.js'
+import validacionRegistro from "./validacionRegistro.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-   
-    if (window.location.pathname === '/auth/register') {
-        validacionRegistro();
-    }
+import validacionConfirmar from "./validacionConfirmar.js";
 
-    if (window.location.pathname === '/auth/login') {
-        // validacionLogin();
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.location.pathname === "/auth/register") {
+    validacionRegistro();
+  }
 
-})
+  if (window.location.pathname === "/auth/confirmar-cuenta") {
+    validacionConfirmar();
+  }
+});
